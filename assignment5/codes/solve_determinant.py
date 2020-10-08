62bf1171a75855186255d87556d14ee20102a2f7
shreeprasadbhat <shreeprasadbt999@gmail.com>
2020-10-08 16:21:47 +0530

    codes for assignment5

--- assignment5/codes/solve_determinant.py
+++ assignment5/codes/solve_determinant.py
@@ -2,7 +2,7 @@ from sympy import Matrix, symbols, Rational, solve
 
 h = symbols('h')
 
-M = Matrix([[6, h, Rational(22,2)], [h, 12, Rational(31,2)],[Rational(22,2), Rational(31,2), 20]])
+M = Matrix([[6, h, 11], [h, 12, Rational(31,2)],[11, Rational(31,2), 20]])
 eq = M.det()
 
 sol = solve(eq, dict=True)
