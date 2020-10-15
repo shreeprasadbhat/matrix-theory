ad46970454963394493593e31b0c26fdffb2446d
shreeprasadbhat <shreeprasadbt999@gmail.com>
2020-10-15 22:20:31 +0530

    programming assignment 2 - gramschmidt

--- programming_assignments/gram_schmidt_orthogonalization/gram_schmidt.py
+++ programming_assignments/gram_schmidt_orthogonalization/gram_schmidt.py
@@ -1,5 +1,5 @@
 """
- Title         : EE5609:Matrix-theory Assignment
+ Title         : EE5609:Matrix-theory Programming Assignment 2
 
  Author        : Shreeprasad Bhat
                  AI20MTECH14011
@@ -18,11 +18,35 @@
 """
 
 import numpy as np
-import copy
-from sympy import Matrix # used for testing purpose
 
-import numpy as np
+def has_zero_vector(M) :
+    """
+    Check for zero vector in input matrix
+    """
+    ncols = M.shape[1]
+    for icol in range(ncols) :
+        if np.all(M[:,icol] == 0) :
+            return True
+    return False
 
+def compute_projection_matrix(v) :
+    """
+    Compute the projection matrix for vector v
+    """
+    v_t = np.transpose(v)
+    dot_prod = np.dot(v.flatten(),v.flatten())
+    P = 0
+    if dot_prod != 0 :
+        P = np.matmul(v,v_t) / dot_prod
+    return P
+
+def projection(u, v) :
+    """
+    Compute the projection of vector v on vector u
+    """
+    P = compute_projection_matrix(u)
+    proj_v = np.dot(P,v)
+    return proj_v
 
 def compute_gram_schmidt(A):
     """
@@ -33,8 +57,26 @@ def compute_gram_schmidt(A):
        2. The ColumnSpace(B) = ColumnSpace(A)
     Both A and B should be np.array
     """
-    # Your code goes here
+    nrows, ncols = A.shape
     
+    B = np.zeros([nrows,ncols])
+
+    # gram-schmidt orthogonalization
+    for icol in range(ncols) :
+        v = A[:,icol].reshape(len(A[:,icol]),1)
+        for jcol in range(icol-1,-1,-1) :
+            u = B[:,jcol].reshape(len(B[:,jcol]),1)
+            v = v - projection(u, v)
+        B[:, icol] = v.flatten()
+
+    # gram-schmidt orthonormalization
+    for icol in range(ncols) :
+        u = B[:, icol]
+        norm_u = np.linalg.norm(u)
+        if norm_u != 0 :
+            B[:, icol] = u/norm_u
+    # we can remove all zero column, since they are linearly dependent on any column
+    B = B[:,~ np.all(B == 0, axis=0)]
     return(B)
 
 """
@@ -45,12 +87,14 @@ Each matrix is a numpy array
 """
 
 test_matrices = np.load('testmatrices.npy',allow_pickle=True)
-
 no_matrices = len(test_matrices)
 
 for i in range(no_matrices):
 
     A = test_matrices[i].copy()
+    if has_zero_vector(A) :
+        print("Matrix has zero column, hence cannot be orthogonalized!!!")
+        continue
     B = compute_gram_schmidt(A)
     print('Matrix ',i)
     testpassed = True
